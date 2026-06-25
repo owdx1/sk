2271b98520d35127b15a978ff2d820c01589dae9
0wdx <0wdx@192.168.1.107>
2026-06-25 16:58:53 +0300

    feat: remote-mcp

---
+++ apps/remote-mcp/src/index.ts
@@ -0,0 +1,62 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
+import { Hono } from "hono"
+import { sendTelegramMessage, telegramMessageInputSchema } from "sendkit-core"
+import { WebStandardStreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/webStandardStreamableHttp.js"
+
+const createServer = (botToken: string) => {
+    const server = new McpServer({
+        name: "sendkit-remote",
+        version: "0.0.0"
+    })
+    server.registerTool("telegram", {
+            title: "telegram",
+            description: "send a telegram message",
+            inputSchema: telegramMessageInputSchema.shape
+        },
+        async (input) => {
+            const result = await sendTelegramMessage({
+                ...input, 
+                botToken
+            })
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: `Sent telegram message ${result.messageId} to chat ${result.chatId}`
+                    }
+                ],
+                structuredContent: result
+            }
+        }
+    )
+    return server;
+}
+
+const app = new Hono();
+
+app.post("/:botToken/mcp", async (c) => {
+    const botToken = c.req.param("botToken");
+    const server = createServer(botToken);
+
+    const transport = new WebStandardStreamableHTTPServerTransport({
+        sessionIdGenerator: undefined,
+        enableJsonResponse: true
+    })
+    await server.connect(transport);
+    try {
+        return await transport.handleRequest(c.req.raw);   
+    } finally {
+        await server.close();
+    }
+})
+
+app.notFound((c) => {
+    return c.json({ error: "Not found. "}, 404);
+})
+
+const port = Number(process.env.PORT || 3000);
+
+export default {
+    port,
+    fetch: app.fetch
+}
